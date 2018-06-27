0fc875cd0254fedb88337a2c9406e30679904821
kda1141 <37535177+kda1141@users.noreply.github.com>
2018-06-27 11:47:23 +0300

    php

---
+++ php/urok10.php
@@ -0,0 +1,13 @@
+<html>
+ </head> 
+   <meta charset="utf-8">
+  <title>10 задание </title>
+  <script>
+  function qwa(a) {return a*a};
+function sum(b,c) {return b+c};
+ </script>
+ </head>
+ <body>
+ 
+ </body>
+</html>
